8f692e51b4fdfb03824aa21f00240c9cde185c39
Uday pratap singh <rchoti83@gmail.com>
2021-09-03 23:07:19 +0530

    commit-2

--- Quiz-Subject-pages/core-sub/DS-page/quiz/Queue/dist/js/userInfo.js
+++ Quiz-Subject-pages/core-sub/DS-page/quiz/Queue/dist/js/userInfo.js
@@ -8,7 +8,18 @@ let Ltime=Lminutes+"m "+Lseconds+"s";
 
 let Lpoints=user_points+"/100";
 
+
+
 document.querySelector("span.name").innerHTML = user_name;
 document.querySelector("span.points").innerHTML = Lpoints;
 document.querySelector("span.time_taken").innerHTML = Ltime;
 
+if(user_points<=30){
+    document.querySelector("span.weldone").innerHTML = "Bad Score! "+user_name;
+}
+else if(user_points<=60){
+    document.querySelector("span.weldone").innerHTML = "Average Score! "+user_name;
+}
+else if(user_points>60){
+    document.querySelector("span.weldone").innerHTML = "Well done! "+user_name;
+}
